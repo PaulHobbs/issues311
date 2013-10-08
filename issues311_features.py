5979334f417e7ee6bdd863bbb11f6b4ebf8084bf
Paul Hobbs <paul.mcdill.hobbs@gmail.com>
2013-10-08 07:30:55 -0700

    Added date to timestamp and time of day functions.  Maybe day of week will be useful too but i don't want to overfit...

--- issues311_features.py
+++ issues311_features.py
@@ -1,6 +1,9 @@
+import calendar
+
 __author__ = 'phobbs'
 
 from operator import itemgetter
+import dateutil.parser
 
 from sklearn import feature_extraction
 
@@ -23,3 +26,27 @@ def vectorizer_features(name, vec=feature_extraction.CountVectorizer):
         ('accessor', FunctionDictTransformer(itemgetter(name))),
         ('vec', DictTransformer(vec()))
     ])
+
+def datetime_to_unix_timestamp(date_time):
+    return calendar.timegm(date_time.utctimetuple())
+
+
+@feature
+@FunctionDictTransformer
+def time_utc(X):
+    return [datetime_to_unix_timestamp(dateutil.parser.parse(row['created_time'])) for row in X]
+
+
+@feature
+@FunctionDictTransformer
+def time_of_day(X):
+    for row in X:
+      created_time = dateutil.parser.parse(row['created_time'])
+      yield datetime_to_unix_timestamp(created_time.time())
+
+
+# TODO handle space differently (with a RBF svm)
+
+# TODO wire these (and others) together
+
+# TODO hyperoptimization
