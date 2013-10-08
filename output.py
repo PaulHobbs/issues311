5833b67d300d9edd42b0156ddd7d2db1c88bbc9b
Paul Hobbs <paul.mcdill.hobbs@gmail.com>
2013-10-08 06:52:57 -0700

    Fixed output.py to use ids and output the right fields.

--- output.py
+++ output.py
@@ -1,9 +1,14 @@
-def output(y, output_file='submission'):
+def output(ids, ys, output_file='submission'):
     """
     Takes the output of several regressors, labels the rows with
     the correct id and writes to a csv file.
     """
     with open('%s.csv' % output_file, 'w') as fp:
         fp.write("id,num_views,num_votes,num_comments")
-        for row in y:
-            fp.write(",".join(row))
+        for i in range(len(ids)):
+            fp.write(",".join((
+                ids[i],
+                ys['num_views'],
+                ys['num_votes'],
+                ys['num_comments']
+            )))
