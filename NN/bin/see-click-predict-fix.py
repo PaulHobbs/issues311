77f8ee67d5438de4fcfadd6f77fd74604fc1ab67
Paul Hobbs <paul.mcdill.hobbs@gmail.com>
2013-10-07 22:28:40 -0700

    Moved stuff around

--- NN/bin/see-click-predict-fix.py
+++
@@ -1,3 +0,0 @@
-__author__ = 'phobbs'
-
-from sklearn import ensemble
--- NN/issues_311/output.py
+++
@@ -1,9 +0,0 @@
-def output(y, output_file='submission'):
-    """
-    Takes the output of several regressors, labels the rows with
-    the correct id and writes to a csv file.
-    """
-    with open('%s.csv' % output_file, 'w') as fp:
-        fp.write("id,num_views,num_votes,num_comments")
-        for row in y:
-            fp.write(",".join(row))
